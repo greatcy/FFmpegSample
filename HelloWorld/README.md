93b94d4d6a3d48ac4c75d451b80eb29dc15e9ab5
chenjunheng <chenjunheng@xunlei.com>
2017-11-24 11:28:10 +0800

    ADD TODO

---
+++ HelloWorld/README.md
@@ -0,0 +1,9 @@
+hello world TODO：
+
+1.CMake config;now I link them to ffmpeg-jni-lib.when load lib it will find other so in nativeLibs.The add library config is redundant.
+2.so lib;can be small.
+
+ffmpeg so is full build.
+so can be packed to a single so.
+
+will be fix in next part;
--- HelloWorld/app/src/main/java/com/eli/helloffmpeg/MainActivity.java
+++ HelloWorld/app/src/main/java/com/eli/helloffmpeg/MainActivity.java
@@ -8,14 +8,6 @@ public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("avutil-55");
-        System.loadLibrary("avcodec-57");
-        System.loadLibrary("avformat-57");
-        System.loadLibrary("avdevice-57");
-        System.loadLibrary("swresample-2");
-        System.loadLibrary("swscale-4");
-        System.loadLibrary("postproc-54");
-        System.loadLibrary("avfilter-6");
         System.loadLibrary("ffmpeg-jni-lib");
     }
 
